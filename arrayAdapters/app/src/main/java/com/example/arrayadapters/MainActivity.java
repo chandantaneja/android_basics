5592b9d8f322796df6d3ad12fca9cd76143825a6
Chandan Taneja <chandan.taneja.931@gmail.com>
2019-04-09 02:18:00 +0530

    Day 01/30 of Android Dev

--- arrayAdapters/app/src/main/java/com/example/arrayadapters/MainActivity.java
+++ arrayAdapters/app/src/main/java/com/example/arrayadapters/MainActivity.java
@@ -2,10 +2,9 @@ package com.example.arrayadapters;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+
 
 
 public class MainActivity extends AppCompatActivity {
